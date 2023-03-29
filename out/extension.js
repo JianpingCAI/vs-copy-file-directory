9265d71e7fc4173be66d26a14dac50751f400ec6
Jianping CAI <cai-jianping@outlook.com>
2023-03-29 14:07:46 +0800

    add "Copy File Relative Directory"

--- out/extension.js
+++ out/extension.js
@@ -5,7 +5,7 @@ exports.deactivate = exports.activate = void 0;
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
 const path = require("path");
-let getPath = function (args) {
+let getFileDirectory = function (args) {
     let filePath = null;
     if (args && args.length > 0) {
         filePath = args[0].fsPath;
@@ -15,19 +15,38 @@ let getPath = function (args) {
     }
     return filePath;
 };
-let pasteAndShowMessage = function (fileDir) {
-    vscode.env.clipboard.writeText(fileDir);
-    vscode.window.setStatusBarMessage(`The file directory "${fileDir}" was copied to the clipboard.`, 3000);
+let getFileRelativeDirectory = function () {
+    const activeDocument = vscode.window.activeTextEditor?.document;
+    if (activeDocument) {
+        const workspaceFolder = vscode.workspace.getWorkspaceFolder(activeDocument.uri);
+        if (workspaceFolder) {
+            const workspaceFolderPath = workspaceFolder.uri.fsPath;
+            const activeDocumentPath = activeDocument.fileName;
+            const relativePath = path.relative(workspaceFolderPath, activeDocumentPath);
+            return relativePath;
+        }
+    }
+    return null;
+};
+let pasteAndShowMessage = function (msg) {
+    vscode.env.clipboard.writeText(msg);
+    vscode.window.setStatusBarMessage(`The file directory "${msg}" was copied to the clipboard.`, 3000);
 };
 function activate(context) {
     let disposable = vscode.commands.registerCommand('copy-file-directory.copyFileDirectory', (...args) => {
-        const fullPath = getPath(args);
+        const fullPath = getFileDirectory(args);
         if (fullPath) {
             const fileDir = path.dirname(fullPath);
             pasteAndShowMessage(fileDir);
         }
     });
     context.subscriptions.push(disposable);
+    disposable = vscode.commands.registerCommand('copy-file-directory.copyRelativeFileDirectory', () => {
+        const relativeDirectory = getFileRelativeDirectory();
+        if (relativeDirectory) {
+            pasteAndShowMessage(relativeDirectory);
+        }
+    });
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 
-let getPath = function (args: vscode.Uri[]): string | null {
+let getFileDirectory = function (args: vscode.Uri[]): string | null {
 	let filePath = null;
 	if (args && args.length > 0) {
 		filePath = args[0].fsPath;
@@ -15,14 +15,29 @@ let getPath = function (args: vscode.Uri[]): string | null {
 	return filePath;
 };
 
-let pasteAndShowMessage = function (fileDir: string) {
-	vscode.env.clipboard.writeText(fileDir);
-	vscode.window.setStatusBarMessage(`The file directory "${fileDir}" was copied to the clipboard.`, 3000);
+let getFileRelativeDirectory = function (): string | null {
+	const activeDocument = vscode.window.activeTextEditor?.document;
+	if (activeDocument) {
+		const workspaceFolder = vscode.workspace.getWorkspaceFolder(activeDocument.uri);
+		if (workspaceFolder) {
+			const workspaceFolderPath = workspaceFolder.uri.fsPath;
+			const activeDocumentPath = activeDocument.fileName;
+			const relativePath = path.relative(workspaceFolderPath, activeDocumentPath);
+			return relativePath;
+		}
+	}
+	return null;
+};
+
+
+let pasteAndShowMessage = function (msg: string) {
+	vscode.env.clipboard.writeText(msg);
+	vscode.window.setStatusBarMessage(`The file directory "${msg}" was copied to the clipboard.`, 3000);
 };
 
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('copy-file-directory.copyFileDirectory', (...args) => {
-		const fullPath = getPath(args);
+		const fullPath = getFileDirectory(args);
 		if (fullPath) {
 			const fileDir = path.dirname(fullPath);
 			pasteAndShowMessage(fileDir);
@@ -30,6 +45,13 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerCommand('copy-file-directory.copyRelativeFileDirectory', () => {
+		const relativeDirectory = getFileRelativeDirectory();
+		if (relativeDirectory) {
+			pasteAndShowMessage(relativeDirectory);
+		}
+	});
 }
 
 // this method is called when your extension is deactivated
