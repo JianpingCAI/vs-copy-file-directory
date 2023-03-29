bb84cca4f3d673399d62028ab70b3cfdd1fdc46a
Jianping CAI <cai-jianping@outlook.com>
2023-03-29 17:19:40 +0800

    add copy file name

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,31 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 
+let getFilePath = function (args: vscode.Uri[]): string | null {
+	let filePath = null;
+	if (args && args.length > 0) {
+		filePath = args[0].fsPath;
+	}
+	if (!filePath) {
+		filePath = vscode.window.activeTextEditor?.document.fileName || null;
+	}
+	return filePath;
+};
+
+let getFileRelativePath = function (): string | null {
+	const activeDocument = vscode.window.activeTextEditor?.document;
+	if (activeDocument) {
+		const workspaceFolder = vscode.workspace.getWorkspaceFolder(activeDocument.uri);
+		if (workspaceFolder) {
+			const workspaceFolderPath = workspaceFolder.uri.fsPath;
+			const activeDocumentPath = activeDocument.fileName;
+			const relativePath = path.relative(workspaceFolderPath, activeDocumentPath);
+			return relativePath;
+		}
+	}
+	return null;
+};
+
 let getFileDirectory = function (args: vscode.Uri[]): string | null {
 	let filePath = null;
 	if (args && args.length > 0) {
@@ -37,14 +62,49 @@ let pasteAndShowMessage = function (msg: string) {
 };
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('copy-file-directory.copyFileDirectory', (...args) => {
+	let disposable = vscode.commands.registerCommand('copy-file-directory.copyFileName', (...args) => {
+		const fullPath = getFilePath(args);
+		if (fullPath) {
+			let extName = path.extname(fullPath);
+            var fileName = path.basename(fullPath, extName);
+			pasteAndShowMessage(fileName);
+		}
+	});
+	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerCommand('copy-file-directory.copyFileNameWithExtension', (...args) => {
+		const fullPath = getFilePath(args);
+		if (fullPath) {
+			let fileName = path.basename(fullPath);
+			pasteAndShowMessage(fileName);
+		}
+	});
+	context.subscriptions.push(disposable);
+
+	
+	disposable = vscode.commands.registerCommand('copy-file-directory.copyFilePath', (...args) => {
+		const filePath = getFilePath(args);
+		if (filePath) {
+			pasteAndShowMessage(filePath);
+		}
+	});
+	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerCommand('copy-file-directory.copyFileRelativePath', (...args) => {
+		const fileRelativePath = getFileRelativePath();
+		if (fileRelativePath) {
+			pasteAndShowMessage(fileRelativePath);
+		}
+	});
+	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerCommand('copy-file-directory.copyFileDirectory', (...args) => {
 		const fullPath = getFileDirectory(args);
 		if (fullPath) {
 			const fileDir = path.dirname(fullPath);
 			pasteAndShowMessage(fileDir);
 		}
 	});
-
 	context.subscriptions.push(disposable);
 
 	disposable = vscode.commands.registerCommand('copy-file-directory.copyRelativeFileDirectory', () => {
@@ -53,6 +113,8 @@ export function activate(context: vscode.ExtensionContext) {
 			pasteAndShowMessage(relativeDirectory);
 		}
 	});
+	context.subscriptions.push(disposable);
+
 }
 
 // this method is called when your extension is deactivated
